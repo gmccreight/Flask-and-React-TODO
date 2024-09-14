4f8dce4b4548b7d10e5a242d761d60a49dc1a721
Gordon McCreight <gmccreight@Gordons-MacBook-Pro.local>
2024-09-14 15:23:26 -0700

    BOLT working in dev container

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3002"],
+    allow_origins=["http://localhost:3002", "http://127.0.0.1:3002"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
